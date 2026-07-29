4b1108eb461ffe4eb5ebfc6b6c28f85aff3c6aa8
xiaooboy <xiaooboy@qq.com>
2026-07-29 16:36:14 +0800

    feat: 调整首页、详情页样式

--- src/views/TracksView.vue
+++ src/views/TracksView.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { computed, onBeforeUnmount, shallowRef } from "vue";
 import { Search } from "@lucide/vue";
-import { useLibraryStore } from "../stores/libraryStore";
-import { usePlayerStore } from "../stores/playerStore";
-import { useFavoriteStore } from "../stores/favoriteStore";
-import { useAlbumStore } from "../stores/albumStore";
-import { useUIStore } from "../stores/uiStore";
+import {
+  useLibraryStore,
+  usePlayerStore,
+  useFavoriteStore,
+  useUIStore,
+  useAlbumStore
+} from "../stores";
 import { useTrackSearch } from "../composables/useTrackSearch";
 import TrackTable from "@/components/TrackTable.vue";
 import SectionHeader from "@/components/SectionHeader.vue";
 import EmptyState from "@/components/EmptyState.vue";
+import { storeToRefs } from "pinia";
 
 const libraryStore = useLibraryStore();
 const playerStore = usePlayerStore();
@@ -17,35 +20,21 @@ const favoriteStore = useFavoriteStore();
 const albumStore = useAlbumStore();
 const uiStore = useUIStore();
 
-const launchTimerId = setTimeout(() => {
+setTimeout(() => {
   launchTimer.value = false;
-}, 100);
+}, 300);
 const launchTimer = shallowRef(true);
+const { loading, tracks, loadingDone, loadingTotal } =
+  storeToRefs(libraryStore);
 
-onBeforeUnmount(() => {
-  clearTimeout(launchTimerId);
-});
-
-const { searchQuery, visibleTracks } = useTrackSearch(
-  () => libraryStore.tracks,
-);
+const { searchQuery, visibleTracks } = useTrackSearch(() => tracks.value);
 
 const emptyTitle = computed(() => {
-  const { loading, tracks } = libraryStore;
   // 避免闪烁
   if (launchTimer.value) return "";
-  if (loading) return "正在整理曲库";
-  if (tracks.length) return "没有匹配到结果";
+  if (loading.value) return "正在整理曲库";
+  if (visibleTracks.value.length) return "没有匹配到结果";
   return "本地曲库未接入";
-});
-const emptyDescription = computed(() => {
-  const { loading, loadingDone, loadingTotal, tracks } = libraryStore;
-  // 避免闪烁
-  if (launchTimer.value) return "";
-  if (loading)
-    return `已处理 ${loadingDone} / ${loadingTotal} 首歌曲，请稍候。`;
-  if (tracks.length) return "没有匹配到结果";
-  return '前往"音乐源"添加。';
 });
 
 function handleSelectTrack(id: string) {
@@ -56,6 +45,9 @@ function handleSelectTrack(id: string) {
 function handleNavigateToAlbum(albumName: string) {
   albumStore.selectAlbum(albumName);
   uiStore.setActiveView("album-detail");
+}
+function handleNavigateToSource() {
+  uiStore.setActiveView("sources");
 }
 </script>
 
@@ -74,17 +66,32 @@ function handleNavigateToAlbum(albumName: string) {
       </label>
     </header>
     <SectionHeader title="歌曲" />
-      <TrackTable
-        v-if="libraryStore.tracks.length"
-        :tracks="visibleTracks"
-        :current-track-id="playerStore.currentTrackId"
-        :is-playing="playerStore.isPlaying"
-        :liked-track-id-set="favoriteStore.likedTrackIdSet"
-        @play="handleSelectTrack"
-        @toggle-play="playerStore.togglePlay"
-        @toggle-favorite="favoriteStore.toggleTrackFavorite"
-        @navigate-to-album="handleNavigateToAlbum"
-      />
-      <EmptyState v-else :title="emptyTitle" :content="emptyDescription"></EmptyState>
+    <TrackTable
+      v-if="libraryStore.tracks.length"
+      :tracks="visibleTracks"
+      :current-track-id="playerStore.currentTrackId"
+      :is-playing="playerStore.isPlaying"
+      :liked-track-id-set="favoriteStore.likedTrackIdSet"
+      @play="handleSelectTrack"
+      @toggle-play="playerStore.togglePlay"
+      @toggle-favorite="favoriteStore.toggleTrackFavorite"
+      @navigate-to-album="handleNavigateToAlbum"
+    />
+    <EmptyState v-else>
+      <template #title v-if="!launchTimer">
+        <strong>
+          {{ loading ? "正在整理曲库" : !tracks.length ? "本地曲库未接入" : "" }}
+        </strong>
+      </template>
+      <template #content v-if="!launchTimer">
+        <p v-if="loading">
+          {{ `已处理 ${loadingDone} / ${loadingTotal} 首歌曲。` }}
+        </p>
+        <p v-else-if="visibleTracks.length">没有匹配到结果</p>
+        <p v-else-if="!tracks.length">
+          前往 <span class="tracks__source-btn" @click="handleNavigateToSource">音乐源</span> 添加歌曲。
+        </p>
+      </template>
+    </EmptyState>
   </div>
 </template>
